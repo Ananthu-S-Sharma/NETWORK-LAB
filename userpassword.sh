4af8aa058a8623d7803ff5b13c479a866c1caeff
Ananthu-S-Sharma <146819078+Ananthu-S-Sharma@users.noreply.github.com>
2024-04-18 20:14:11 +0530

    Add files via upload

---
+++ userpassword.sh
@@ -0,0 +1,10 @@
+valid_username="user"
+valid_password="ananthumca"
+read -p "Enter your username: " username
+read -sp "Enter your password: " password
+echo
+if [[ $username == $valid_username && $password == $valid_password ]]; then
+    echo "Login successful!"
+else
+    echo "Login failed!"
+fi
